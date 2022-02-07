0bc8a17a91ab8faeecfc8cb127d8b9861588e2e3
Gabriel Luis Nolasco <94998727+BielIl@users.noreply.github.com>
2022-02-07 08:57:58 -0300

    Update usuario model
     Relacionar @ManyToOne com Usuario
     Relacionar @ManyToOne com Tema
     Colocar notação @JsonIgnoreProperties no atributo usuario
     Colocar notação @JsonIgnoreProperties no atributo tema
     Validar todos Getters e Setters

--- src/main/java/org/generation/blogpessoal/model/Usuario.java
+++ src/main/java/org/generation/blogpessoal/model/Usuario.java
@@ -16,17 +16,36 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotNull
+	@NotNull(message = "O atributo Nome é Obrigatório!")
 	@Size(min = 3, max = 100)
 	private String nome;
+	
+	@Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caractéres")
+	private String foto;
 
-	@NotNull
-	@Size(min = 5, max = 100)
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 
-	@NotNull
-	@Size(min = 5, max = 100)
+	@NotBlank(message = "O atributo Senha é Obrigatório!")
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
 	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public Usuario(Long id, String nome, String foto, String usuario, String senha) {
+		this.id = id;
+		this.nome = nome;
+		this.foto = foto;
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+	
+	public Usuario() {}
+	
+	/* Getters and Setters */
 
 	public Long getId() {
 		return id;
@@ -58,6 +77,14 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 
 }
