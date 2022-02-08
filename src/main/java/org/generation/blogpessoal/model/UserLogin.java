6d0f8d219da0dcbdbde576ed50941135571023f2
Gabriel Luis Nolasco <94998727+BielIl@users.noreply.github.com>
2022-02-08 07:51:52 -0300

    Update UserLogin.java

--- src/main/java/org/generation/blogpessoal/model/UserLogin.java
+++ src/main/java/org/generation/blogpessoal/model/UserLogin.java
@@ -1,6 +1,8 @@
 package org.generation.blogpessoal.model;
 
 public class UserLogin {
+	
+	private long id;
 
 	private String nome;
 
@@ -11,6 +13,14 @@ public class UserLogin {
 	private String token;
 
 	/* Getters and Setters */
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 	
 	public String getNome() {
 		return nome;
